84dcdf29ecfe9cde7e55586f3864d90acf10abe2
Youssef-Abo-El-Ela <youssefmm2003@gmail.com>
2025-07-20 18:19:26 +0300

    Update package dependencies and integrate Swagger UI for API documentation

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import app from "./app";
 import { PORT } from "./config/env";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
+const swaggerDocument = YAML.load("../swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
