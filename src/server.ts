33dc16e39c0c43386814192fe201ebe9e930db14
Youssef-Abo-El-Ela <youssefmm2003@gmail.com>
2025-07-20 18:58:34 +0300

    moved  swagger.yaml file and update server configuration to load Swagger documentation from the correct path.

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { PORT } from "./config/env";
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
-const swaggerDocument = YAML.load("./swagger.yaml");
+const swaggerDocument = YAML.load("./src/swagger.yaml");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
